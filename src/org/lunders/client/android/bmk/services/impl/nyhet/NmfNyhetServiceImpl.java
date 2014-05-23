805910c8a88d24070ea30a8ac9fcccfbf78a0af6
Anders Lund <anders.lund@gjensidige.no>
2014-05-23 14:36:02 +0200

    Copyright

--- src/org/lunders/client/android/bmk/services/impl/nyhet/NmfNyhetServiceImpl.java
+++ src/org/lunders/client/android/bmk/services/impl/nyhet/NmfNyhetServiceImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Anders Lund
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lunders.client.android.bmk.services.impl.nyhet;
 
 import android.util.Log;
@@ -9,14 +25,6 @@ import org.lunders.client.android.bmk.services.impl.AbstractServiceImpl;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Copyright (c) 2014 - Gjensidige Forsikring ASA
- * All rights reserved
- * <p/>
- * www.gjensidige.com
- *
- * @author G009430
- */
 public class NmfNyhetServiceImpl extends AbstractServiceImpl implements NyhetService {
 
 	private static final String TAG = NmfNyhetServiceImpl.class.getSimpleName();
--- src/org/lunders/client/android/bmk/util/DateUtil.java
+++ src/org/lunders/client/android/bmk/util/DateUtil.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Anders Lund
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.lunders.client.android.bmk.util;
 
 import java.text.ParsePosition;
@@ -5,14 +21,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * Copyright (c) 2014 - Gjensidige Forsikring ASA
- * All rights reserved
- * <p/>
- * www.gjensidige.com
- *
- * @author G009430
- */
 public class DateUtil {
 
 	private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
